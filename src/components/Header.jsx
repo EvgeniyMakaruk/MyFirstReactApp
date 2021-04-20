46e91b70653329f49d7a7bda08c3bed51d20942d
MakarukEvgeniy <makarukssd@mail.ru>
2021-04-20 18:08:58 +0300

    Первое сохранинение

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+
+const Header=()=>{
+   return(<header className='header'>
+        <img src='https://clck.ru/UPF73'></img>
+     </header>)
+};
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+
+const Profile=()=>{
+     return(
+      <div className='content'>
+      <div>
+      <img src='https://clck.ru/UPH3d'></img>
+      </div>
+      <div>
+         ava+description
+      </div>
+
+       <div>
+          My posts
+          <div>
+             nev post
+          </div>
+          <div>
+             <div>
+                post1
+             </div>
+             <div>
+                post2
+             </div>
+          </div>
+       </div>
+
+   </div>
+     )
+}
+
+export default Profile
