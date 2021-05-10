322e1ce495a5c412c8250e28e42e6bf4844eb67b
EvgeniyMakaruk <makarukssd@mail.ru>
2021-05-10 14:59:01 +0300

    48

--- src/Redux/dialogs-reducer.jsx
+++ src/Redux/dialogs-reducer.jsx
@@ -1,48 +1,48 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY'
 const SEND_MESSAGE = 'SEND_MESSAGE'
 
-let initialState={
+let initialState = {
    messages: [
-     { id: 1, message: 'Hi' },
-     { id: 2, message: 'How are you' },
-     { id: 3, message: 'Hope so' },
-     { id: 4, message: 'Yooo' },
-     { id: 5, message: 'Yooo' }
+      { id: 1, message: 'Hi' },
+      { id: 2, message: 'How are you' },
+      { id: 3, message: 'Hope so' },
+      { id: 4, message: 'Yooo' },
+      { id: 5, message: 'Yooo' }
    ],
    dialogs: [
-     { id: 1, name: 'Dimych' },
-     { id: 2, name: 'Andrey' },
-     { id: 3, name: 'Menko' },
-     { id: 4, name: 'Zisic' },
-     { id: 5, name: 'Vasya' },
-     { id: 6, name: 'Petya' }
+      { id: 1, name: 'Dimych' },
+      { id: 2, name: 'Andrey' },
+      { id: 3, name: 'Menko' },
+      { id: 4, name: 'Zisic' },
+      { id: 5, name: 'Vasya' },
+      { id: 6, name: 'Petya' }
    ],
    newMessageBody: ''
- }
+}
 
-const dialogsReducer = (state=initialState, action) => {
-
-   let stateCopy={
-      ...state
-   }
+const dialogsReducer = (state = initialState, action) => {
    
    switch (action.type) {
       case UPDATE_NEW_MESSAGE_BODY:
-         stateCopy.newMessageBody = action.body
-         return stateCopy
+         return{
+            ...state,
+            newMessageBody: action.body
+         }
       case SEND_MESSAGE:
          let body = state.newMessageBody
-         stateCopy.newMessageBody = ''
-         stateCopy.messages.push({ id: 6, message: body });
-         return stateCopy
-         default:
-            return state
+         return {
+            ...state,
+            newMessageBody: '',
+            messages: [...state.messages, { id: 6, message: body }]
+         }
+      default:
+         return state
    }
 }
 
 
 export const sendMessageCreator = () => ({ type: SEND_MESSAGE })
 export const updateNewMessageBodyCreator = (body) =>
-  ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
+   ({ type: UPDATE_NEW_MESSAGE_BODY, body: body })
 
 export default dialogsReducer
--- src/Redux/profile-reducer.jsx
+++ src/Redux/profile-reducer.jsx
@@ -2,42 +2,44 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
-let initialState={
+let initialState = {
    posts: [
-     { id: 1, message: 'Hi how are you', likeCount: 12 },
-     { id: 2, message: 'It s my first post', likeCount: 11 }
+      { id: 1, message: 'Hi how are you', likeCount: 12 },
+      { id: 2, message: 'It s my first post', likeCount: 11 }
    ],
    newPostText: 'it-Kamasutra.com'
 
- }
+}
 
-const profileReducer = (state=initialState, action) => {
+const profileReducer = (state = initialState, action) => {
    switch (action.type) {
-      case ADD_POST:{ let newPost = {
-         id: 5,
-         message: state.newPostText,
-         likeCount: 0
-      }
-      let stateCopy={...state};
-         state.posts=[...state.posts]
-         stateCopy.posts.push(newPost)
-         stateCopy.newPostText = ''
-         return stateCopy
+      case ADD_POST: {
+         let newPost = {
+            id: 5,
+            message: state.newPostText,
+            likeCount: 0
+         }
+         return{
+            ...state,
+            posts: [...state.posts, newPost],
+            newPostText: ''
+         }
       }
 
-      case UPDATE_NEW_POST_TEXT:{
-         let stateCopy={...state}
-         stateCopy.newPostText = action.newText;
-         return stateCopy
+      case UPDATE_NEW_POST_TEXT: {
+        return{
+           ...state,
+           newPostText:action.newText
+        }
       }
       default:
          return state
-      
+
    }
 }
 
 export const addPostActionCreator = () => ({ type: ADD_POST })
 export const updateNewPostText = (text) =>
-  ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+   ({ type: UPDATE_NEW_POST_TEXT, newText: text })
 
 export default profileReducer
