49a2bea43dbaa3e86808ac07cb5102f95553ad4b
EvgeniyMakaruk <makarukssd@mail.ru>
2021-05-17 22:17:56 +0300

    57

--- src/components/Users/Users-container.jsx
+++ src/components/Users/Users-container.jsx
@@ -1,62 +1,84 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC } from '../../Redux/users-reducer copy'
-import * as axios from'axios'
+import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, toggleIsFetchingAC, unfollowAC } from '../../Redux/users-reducer copy'
+import * as axios from 'axios'
 import Users from './Users'
+import Preloader from '../common/Preloader/Preloader'
+
+
+
+
+
+
 
 class UsersContainer extends React.Component {
    componentDidMount() {
-      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-         this.props.setUsers(response.data.items)
-         this.props.setTotalUsersCount(response.data.totalCount)
-      })
+      this.props.toggleIsFetchin(true)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+         .then(response => {
+            this.props.toggleIsFetchin(false)
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+         })
    }
 
    onPageChanged = (pageNumber) => {
       this.props.setCurrentPage(pageNumber)
+      this.props.toggleIsFetchin(true)
       axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=
       ${pageNumber}&count=
-      ${this.props.pageSize}`).then(response => {
-         this.props.setUsers(response.data.items)
-      })
+      ${this.props.pageSize}`)
+         .then(response => {
+            this.props.toggleIsFetchin(false)
+            this.props.setUsers(response.data.items)
+         })
    }
 
    render() {
-      return <Users totalUsersCount={this.props.totalUsersCount}
-         pageSize={this.props.pageSize}
-         currentPage={this.props.currentPage}
-         onPageChanged={this.onPageChanged}
-         users={this.props.users}
-         follow={this.props.follow}
-         unfollow={this.props.unfollow}
-      />
+      return <>
+         {this.props.isFetching ? <Preloader/> : null}
+         <Users totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+         />
+      </>
    }
 }
 
-let mapStateToProps=(state)=>{
+let mapStateToProps = (state) => {
+
    return {
       users: state.usersPage.users,
       pageSize: state.usersPage.pageSize,
       totalUsersCount: state.usersPage.totalUsersCount,
-      currentPage:state.usersPage.currentPage
-   }}
+      currentPage: state.usersPage.currentPage,
+      isFetching: state.usersPage.isFetching
+   }
+}
 
-let mapDispatchToProps=(dispatch)=>{
-   return{
-      follow:(userId)=>{
+let mapDispatchToProps = (dispatch) => {
+   return {
+      follow: (userId) => {
          dispatch(followAC(userId))
       },
-      unfollow:(userId)=>{
+      unfollow: (userId) => {
          dispatch(unfollowAC(userId))
       },
-      setUsers:(users)=>{
+      setUsers: (users) => {
          dispatch(setUsersAC(users))
       },
-      setCurrentPage:(pageNumber)=>{
+      setCurrentPage: (pageNumber) => {
          dispatch(setCurrentPageAC(pageNumber))
       },
-      setTotalUsersCount:(totalCount)=>{
+      setTotalUsersCount: (totalCount) => {
          dispatch(setTotalUsersCountAC(totalCount))
+      },
+      toggleIsFetchin: (isFetching) => {
+         dispatch(toggleIsFetchingAC(isFetching))
       }
    }
 }
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,8 @@
+import preLoader from '../../../assets/img/preLoader.gif'
+import React from 'react'
+
+let Preloader =(props)=>{
+   return <img src={preLoader} />
+}
+
+export default Preloader
