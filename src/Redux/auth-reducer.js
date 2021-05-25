53127ded1562a484323442a15f8ac6f5b506605f
EvgeniyMakaruk <makarukssd@mail.ru>
2021-05-25 20:31:33 +0300

    67

--- src/Redux/auth-reducer.js
+++ src/Redux/auth-reducer.js
@@ -1,3 +1,5 @@
+import { authAPI } from "../api/api"
+
 const SET_USER_DATA = 'SET-USER-DATA'
 
 
@@ -5,7 +7,7 @@ let initialState = {
    userId: null,
    email: null,
    login: null,
-   isAuth:false
+   isAuth: false
 }
 
 const authReducer = (state = initialState, action) => {
@@ -22,5 +24,15 @@ const authReducer = (state = initialState, action) => {
 }
 
 export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } })
+
+export const getAuthUserData = () => (dispatch) => {
+   authAPI.me()
+      .then(response => {
+         if (response.data.resultCode === 0) {
+            let { id, email, login } = response.data.data
+            dispatch(setAuthUserData(id, email, login))
+         }
+      })
+}
 
 export default authReducer
--- src/Redux/profile-reducer.jsx
+++ src/Redux/profile-reducer.jsx
@@ -1,3 +1,4 @@
+import { usersAPI } from "../api/api"
 
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
@@ -49,5 +50,11 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({ type: ADD_POST })
 export const updateNewPostText = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text })
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
+
+export const getUserProfile = (userId) => (dispatch) => {
+   usersAPI.getProfile(userId).then(response => {
+      dispatch(setUserProfile(response.data))
+   })
+}
 
 export default profileReducer
