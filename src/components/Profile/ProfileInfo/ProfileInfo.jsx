2c787f8f9980bb740d78f42efe8ddf491d19e45f
EvgeniyMakaruk <makarukssd@mail.ru>
2021-05-27 23:16:37 +0300

    71

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,8 @@
 import Profile from '../Profile';
 import s from'./ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader'
+import ProfileStatus from './ProfileStatus';
+
 const ProfileInfo=(props)=>{
    if (!props.profile){
       return <Preloader/>
@@ -8,14 +10,15 @@ const ProfileInfo=(props)=>{
 
    return(
       <div>
-         <div>
+         {/* <div>
             <img src="https://clck.ru/UPH3d" alt=""></img>
-         </div>
+         </div> */}
 
       <div className={s.DescriptionBlock}>
          <img src={props.profile.photos.large}/>
          <div>{props.profile.aboutMe}</div>
          <div>{'Мой twitter '+props.profile.contacts.twitter}</div>
+         <ProfileStatus status='Hello world'/>
       </div>
       </div>
    )
